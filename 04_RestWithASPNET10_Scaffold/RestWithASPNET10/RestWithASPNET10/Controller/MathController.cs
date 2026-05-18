6bb3d1800c96e376f194f79c2f0fe31a70d22ab0
Pedro da Costa Lopes <pedrolopes.programador013@gmail.com>
2026-05-18 13:08:00 -0300

    Desenvolvimento dos modulos de IsNumber e ConvertToDecimal, e desenvolvimento da API de soma

---
+++ 04_RestWithASPNET10_Scaffold/RestWithASPNET10/RestWithASPNET10/Controller/MathController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace RestWithASPNET10.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum);
+            }
+            
+            
+            return BadRequest("Invalid Input");
+        }
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            decimal decimalValue;
+            bool isNumber = decimal.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue
+                );
+
+            return isNumber;
+        }
+    }
+}
